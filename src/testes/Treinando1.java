1d6785e425bd79bc53297c58a8336f60efdfd045
51115152017.2 <51115152017.2@51DE194266.senacrjedu.local>
2017-09-20 17:04:06 -0300

    aula dia 20/09

---
+++ src/testes/Treinando1.java
@@ -0,0 +1,19 @@
+package testes;
+
+public class Treinando1 {
+
+	public static void main(String[] args) {
+		
+		Integer x;
+		
+				
+		
+		for(x=1; x <= 13; x++) {
+			System.out.println("Eu Irei Programar !");
+		} 
+			
+		
+		
+	}
+
+}
---
+++ src/testes/treinando2.java
@@ -0,0 +1,19 @@
+package testes;
+
+public class treinando2 {
+
+	public static void main(String[] args) {
+		int i;
+		int soma;
+		
+		i = 30;
+		soma = 0;
+		while (i >= 0) {
+			soma = soma + i;
+			--i;
+		}
+		System.out.println(soma);
+
+	}
+
+}
